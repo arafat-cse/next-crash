2465eeb22f8f93469d1d0c296c7ac574b34fec5b
Arafat <arafat.developer2008@gmail.com>
2026-02-10 01:44:31 +0600

    feat: create Blogs page with blog list and layout

--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -1,11 +1,27 @@
+import { time } from "console";
 import React from "react";
 
+
 export default function Blogs(){
+  const blogs = [
+    {
+      id: 1,
+      title:"Blog 1",
+      description: "Blog 1 description",
+    },{
+      id: 2,
+      title:"Blog 2",
+      description:"Blog 2 description",
+    }
+  ];
   return(
-    <main>
+    <main className="mt-10">
       <div>
         Blog page hera
       </div>
+      <ul>
+        {blogs.map((blog) => (<li key={blog.id}>{blog.title}</li>))}
+      </ul>
     </main>
   )
 }
